cf497adb33e80d364f7412181edba6a2ed6756ad
SilvesterSpath <spath.sz@gmail.com>
2023-08-10 06:07:07 +0200

    typescript error 1

--- models/User.ts
+++ models/User.ts
@@ -1,4 +1,5 @@
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 
 export interface UserProps {
   name?: string;
@@ -7,6 +8,7 @@ export interface UserProps {
 }
 
 export class User {
+  // we can't easily swap this, but here is okay, not with Sync
   public events: Eventing = new Eventing();
 
   constructor(private data: UserProps) {}
