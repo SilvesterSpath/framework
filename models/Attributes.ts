1f2ae9c82bb89ec66a12e5b5b15926af1b99f48a
SilvesterSpath <spath.sz@gmail.com>
2023-09-05 06:06:13 +0200

    keys of

--- models/Attributes.ts
+++ models/Attributes.ts
@@ -1,11 +1,25 @@
-export class Attributes<Type> {
-  constructor(private data: Type) {}
+import { UserProps } from './User';
 
-  get(propName: string): string | number {
+export class Attributes<T extends {}> {
+  constructor(private data: T) {}
+
+  get(propName: string): string | number | boolean {
     return this.data[propName];
   }
 
-  set(update: Type): void {
+  set(update: T): void {
     Object.assign(this.data, update);
   }
 }
+
+const attr = new Attributes<UserProps>({ id: 5, name: 'Silvester', age: 40 });
+
+const id = attr.get('id') as number;
+
+if (typeof id === 'number') {
+  id;
+}
+
+type BestName = 'silvester';
+
+const printName = (name: BestName): void => {};
