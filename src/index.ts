fd2a009aa8fd7aa2152b60a67c8725b2eabc37ae
SilvesterSpath <spath.sz@gmail.com>
2023-09-06 09:47:57 +0200

    using refactored version of User

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,8 @@
-/* import axios from 'axios';
-
-axios.post('http://localhost:3000/users', {
-  name: 'Silvester',
-  age: 20,
-});
-
-const getUser = async () => {
-  const user = await axios.get('http://localhost:3000/users/1');
-  console.log(user.data);
-};
-
-getUser(); */
-
 import { User } from '../models/User';
-import { Sync } from '../models/Sync';
 
-const user = new User({ id: 1 });
-const sync = new Sync('http://localhost:3000/users');
+const user = new User({ name: 'Silvester', age: 47, id: 3 });
+user.attributes.get('name');
+user.attributes.get('age');
+user.attributes.get('id');
 
-const id = user.get('id');
-
-sync.fetch(id);
-
-user.set({ name: 'Swx' });
-user.set({ age: 40 });
-
-user.events.on('change', () => {
-  console.log('change!');
-});
-
-user.events.trigger('change');
-
-/* user.fetch();
-setTimeout(() => {
-  console.log(user);
-}, 4000); */
+user.sync.save();
