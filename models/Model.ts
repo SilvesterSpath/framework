feb052cf654a064fc35d2f9db4fcfa8d30c7c4a5
SilvesterSpath <spath.sz@gmail.com>
2023-10-09 10:46:29 +0200

    refactor Model and User

--- models/Model.ts
+++ models/Model.ts
@@ -1,4 +1,4 @@
-import { AxiosPromise } from 'axios';
+import { AxiosPromise, AxiosResponse } from 'axios';
 
 interface ModelAttributes<T> {
   get<K extends keyof T>(key: K): T[K];
@@ -16,4 +16,62 @@ interface Events {
   trigger(eventName: string): void;
 }
 
-export class Model {}
+interface HasId {
+  id?: number;
+}
+
+export class Model<T extends HasId> {
+  constructor(
+    private attributes: ModelAttributes<T>,
+    private events: Events,
+    private sync: Sync<T>
+  ) {}
+
+  // instead of this, we simple return the this.events with get()
+  // not calling only returning a reference
+  /* on(eventName: string, callback: Callback): void {
+    this.events.on(eventName, callback)
+  } */
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  // with using an additional 'get' we don't need to use parentheses calling get method
+  get get() {
+    return this.attributes.get;
+  }
+
+  set(update: T): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id');
+    }
+
+    this.sync.fetch(id).then((res: AxiosResponse): void => {
+      this.set(res.data);
+    });
+  }
+
+  save(): void {
+    const data = this.attributes.getAll();
+    this.sync
+      .save(data)
+      .then((res: AxiosResponse): void => {
+        this.trigger('save');
+      })
+      .catch((err) => {
+        this.trigger('error');
+      });
+  }
+}
--- models/User.ts
+++ models/User.ts
@@ -1,7 +1,5 @@
-import { AxiosResponse } from 'axios';
 import { Attributes } from './Attributes';
-import { Eventing } from './Eventing';
-import { Sync } from './Sync';
+import { Model } from './Model';
 
 export interface UserProps {
   name?: string;
@@ -11,60 +9,4 @@ export interface UserProps {
 
 const rootUrl = 'http://localhost:3000/users';
 
-export class User {
-  // we can't easily swap this, but here is okay, not with Sync
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public attributes: Attributes<UserProps>;
-
-  constructor(data: UserProps) {
-    this.attributes = new Attributes<UserProps>(data);
-  }
-
-  // instead of this, we simple return the this.events with get()
-  // not calling only returning a reference
-  /* on(eventName: string, callback: Callback): void {
-    this.events.on(eventName, callback)
-  } */
-  get on() {
-    return this.events.on;
-  }
-
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  // with using an additional 'get' we don't need to use parentheses calling get method
-  get get() {
-    return this.attributes.get;
-  }
-
-  set(update: UserProps): void {
-    this.attributes.set(update);
-    this.events.trigger('change');
-  }
-
-  fetch(): void {
-    const id = this.get('id');
-
-    if (typeof id !== 'number') {
-      throw new Error('Cannot fetch without an id');
-    }
-
-    this.sync.fetch(id).then((res: AxiosResponse): void => {
-      this.set(res.data);
-    });
-  }
-
-  save(): void {
-    const data = this.attributes.getAll();
-    this.sync
-      .save(data)
-      .then((res: AxiosResponse): void => {
-        this.trigger('save');
-      })
-      .catch((err) => {
-        this.trigger('error');
-      });
-  }
-}
+export class User extends Model<UserProps> {}
