74f8385f087f17f6597a48313f7dd72d0863ea08
SilvesterSpath <spath.sz@gmail.com>
2023-11-13 06:34:57 +0100

    bypassing type union

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -5720,19 +5720,26 @@ exports.Collection = Collection;
 },{"axios":"node_modules/axios/index.js","./Eventing":"models/Eventing.ts"}],"models/User.ts":[function(require,module,exports) {
 "use strict";
 
-function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
-function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); Object.defineProperty(subClass, "prototype", { writable: false }); if (superClass) _setPrototypeOf(subClass, superClass); }
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+var __extends = this && this.__extends || function () {
+  var _extendStatics = function extendStatics(d, b) {
+    _extendStatics = Object.setPrototypeOf || {
+      __proto__: []
+    } instanceof Array && function (d, b) {
+      d.__proto__ = b;
+    } || function (d, b) {
+      for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p];
+    };
+    return _extendStatics(d, b);
+  };
+  return function (d, b) {
+    if (typeof b !== "function" && b !== null) throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+    _extendStatics(d, b);
+    function __() {
+      this.constructor = d;
+    }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+  };
+}();
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -5747,46 +5754,33 @@ var rootUrl = 'http://localhost:3000/users';
  * Defines the User class which extends the generic Model class.
  * The User class represents a user data model with typed props.
  */
-var User = /*#__PURE__*/function (_Model_1$Model) {
-  _inherits(User, _Model_1$Model);
-  var _super = _createSuper(User);
+var User = /** @class */function (_super) {
+  __extends(User, _super);
   function User() {
-    _classCallCheck(this, User);
-    return _super.apply(this, arguments);
+    return _super !== null && _super.apply(this, arguments) || this;
   }
-  _createClass(User, [{
-    key: "isAdminUser",
-    value: function isAdminUser() {
-      return this.get('id') === 1;
-    }
-  }, {
-    key: "setRandomAge",
-    value: function setRandomAge() {
-      var age = Math.round(Math.random() * 100);
-      this.set({
-        age: age
-      });
-    }
-  }, {
-    key: "setRandomName",
-    value: function setRandomName() {
-      var input = document.querySelector('input[name="name"]');
-      var name = input.value;
-      this.set({
-        name: name
-      });
-    }
-  }], [{
-    key: "buildUser",
-    value: function buildUser(attrs) {
-      return new User(new Attributes_1.Attributes(attrs), new Eventing_1.Eventing(), new ApiSync_1.ApiSync(rootUrl));
-    }
-  }, {
-    key: "buildUserCollection",
-    value: function buildUserCollection() {
-      return new Collection_1.Collection(rootUrl, User.buildUser);
-    }
-  }]);
+  User.buildUser = function (attrs) {
+    return new User(new Attributes_1.Attributes(attrs), new Eventing_1.Eventing(), new ApiSync_1.ApiSync(rootUrl));
+  };
+  User.prototype.isAdminUser = function () {
+    return this.get('id') === 1;
+  };
+  User.buildUserCollection = function () {
+    return new Collection_1.Collection(rootUrl, User.buildUser);
+  };
+  User.prototype.setRandomAge = function () {
+    var age = Math.round(Math.random() * 100);
+    this.set({
+      age: age
+    });
+  };
+  User.prototype.setRandomName = function () {
+    var input = document.querySelector('input[name="name"]');
+    var name = input.value;
+    this.set({
+      name: name
+    });
+  };
   return User;
 }(Model_1.Model);
 exports.User = User;
